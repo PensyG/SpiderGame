d7075f39ccb0d932e65ef91833c890688cc47150
PensyG <38053976+PensyG@users.noreply.github.com>
2018-04-05 19:06:06 -0600

    Update Fly.java

--- Fly.java
+++ Fly.java
@@ -4,7 +4,8 @@ import java.util.Random;
 /**
  * Simulates a fly caught in a web, able to vibrate, and be eaten by a spider
  */
-public class Fly {
+public class Fly
+{
 
     private static Random random = new Random();
     private static ArrayList<Fly> flies = new ArrayList<>();    //Fly array
@@ -24,17 +25,19 @@ public class Fly {
     private FlySize size;               //size of fly, larger = bigger vibrations
 
     /**
+     * This constructor creates a new fly in the web
      * @param locationRow_ - row of the web (i)
      * @param locationCol_ - column of the web (j)
      */
-    public Fly(int locationRow_, int locationCol_, FlySize size_) {
+    public Fly(int locationRow_, int locationCol_, FlySize size_)
+    {
         locationRow = locationRow_;                     //new (i) coordinate
         locationCol = locationCol_;                     //new (j) coordinate
         size = size_;                                   //FlySize
         vibrateEnergy = (size.ordinal() + 1) * 5;       //vibrate more the larger the size (10, 15, 20)
         foodValue = (size.ordinal() + 1) * 2;           //For each size, give 2 health when eaten
         scoreValue = (size.ordinal()+ 1) * 5;           //for each size, give 5 score when eaten
-    }
+    }//end of Fly constructor
 
     /**
      * variable fly size, looks at the max amounts of each fly that can be created,
@@ -52,121 +55,144 @@ public class Fly {
                 return Large;
             else
                 return null;
-        }
-    }
+        }//end of FlySize method
+    }//end of FlySize
 
     /**
-     * setup class variables before each game
+     * The setConstants method sets up class variables before each game
      * @param webSize - Current diameter of the web (50, 100, 200, 400)
      */
-    public static void setConstants(int webSize) {
+    public static void setConstants(int webSize)
+    {
         //number of 50x50 quadrants in web
         int numberQuadrants = (int) Math.pow((webSize / 50), 2);
 
         MAX_SMALL = (int) (6.5 * numberQuadrants);
         MAX_MEDIUM = (int) (3.33 * numberQuadrants);
         MAX_LARGE = (int) (1.2 * numberQuadrants);
-    }
+    }//end of setConstants method
 
     /**
-     * setup adjust variables each turn, the game regenerates less flies as it continues
+     * The adjustConstants method sets up adjust variables each turn
+     * The game regenerates less flies as it continues
      */
-    public static void adjustConstants() {
+    public static void adjustConstants()
+    {
         double reductionConstant = .95;
 
         MAX_SMALL *= reductionConstant;
         MAX_MEDIUM *= reductionConstant;
         MAX_LARGE *= reductionConstant;
-    }
+    }//end of adjustConstants method
 
     /**
+     * The getFoodValue returns the Fly's food value for spider
      * @return - the amount of health the Fly is worth
      */
-    public int getFoodValue() {
+    public int getFoodValue()
+    {
         return foodValue;
-    }
+    }//end of getFoodValue method
 
     /**
+     * The getScoreValue returns the fly's value
      * @return - The amount of Game.score the Fly is worth
      */
-    public int getScoreValue() {
+    public int getScoreValue()
+    {
         return scoreValue;
-    }
+    }//end of getScoreValue method
 
     /**
+     * The getRow method returns the fly's row in the web
      * @return - The current row of the Fly (i)
      */
-    public int getRow() {
+    public int getRow()
+    {
         return locationRow;
-    }
+    }//end of getRow method
 
     /**
+     * The getCol method returns the fly's column in the web
      * @return - The current Column of the Fly (j)
      */
-    public int getCol() {
+    public int getCol()
+    {
         return locationCol;
-    }
+    }//end of getCol method
 
     /**
+     * The getEnergy method returns the value of the fly's vibration in the web
      * @return - The vibration value, 5, 10, or 15 based on current sizes
      */
-    public int getEnergy() {
+    public int getEnergy()
+    {
         return vibrateEnergy;
-    }
+    }//end of getEnergy method
 
     /**
+     * The getSize method returns the size of the fly
      * @return - Small, Medium, Large
      */
-    public FlySize getSize() {
+    public FlySize getSize()
+    {
         return size;
-    }
+    }//end of getSize method
 
     /**
+     * The getFlies method returns the fly ArrayList
      * @return - the entire Fly array
      */
-    public static ArrayList<Fly> getFlies() {
+    public static ArrayList<Fly> getFlies()
+    {
         return flies;
-    }
+    }//end of getFlies method
 
     /**
+     * The getMaxEnergy method sets the formatting of the web
      * used for formatting the display of vibration values (so it's all the same value)
      * @return - the fly
      */
-    public static int getMaxEnergy() {
+    public static int getMaxEnergy()
+    {
         int maxEnergy = 0;
-        for (Fly fly : flies) {
+        for (Fly fly : flies)
+        {
             //find max energy to format the web display
             if (maxEnergy < fly.getEnergy())
                 maxEnergy = fly.getEnergy();
-        }
+        }//end of enhanced for loop to find max energy in array
         return maxEnergy;
-    }
+    }//end of getMaxEnergy method
 
     /**
-     * checks if current element (i, j) is a fly
+     * The getFly method checks if current element (i, j) is a fly
      *
      * @param i - row of Web array
      * @param j - column of Web array
      * @return the fly object in that location, or null if it's empty
      */
-    public static Fly getFly(int i, int j) {
+    public static Fly getFly(int i, int j)
+    {
         //location matches a Fly in the flies array
         for (Fly fly : flies)
             if (i == fly.getRow() && j == fly.getCol())
                 return fly;
         return null;
-    }
+    }//end of getFly method
 
     /**
-     * Updates the amount of each size of Fly in the web, is called in generateFlies to make sure the
-     * max amount of flies are in the web each turn
+     * The getCurrentFlies method updates the amount of each size of Fly in the web, is called in
+     * generateFlies to make sure the max amount of flies are in the web each turn
      */
-    private static void getCurrentFlies() {
+    private static void getCurrentFlies()
+    {
         currentSmall = 0;
         currentMedium = 0;
         currentLarge = 0;
         for (Fly fly : flies)
-            switch (fly.getSize()) {
+            switch (fly.getSize())
+            {
                 case Small:
                     currentSmall++;
                     break;
@@ -179,72 +205,83 @@ public class Fly {
                 default:
                     System.out.println("Error in getCurrentFlies()");
                     break;
-            }
-    }
+            }//end of fly.getSize switch
+    }//end of getCurrentFlies method
 
     /**
-     * updates the current amount of Fly objects in the array
+     * The update method updates the current amount of Fly objects in the array
      * @param web - Web object, passed into the generateFlies function
      */
-    public static void update(Web web) {
+    public static void update(Web web)
+    {
         Fly.getCurrentFlies();
         generateFlies(web);
 
         //debug method
         if (Game.DEBUG)
             debug();
-    }
+    }//end of update method
 
     /**
-     * Generates a fly in a currently unoccupied space
+     * The generateFlies method generates a fly in a currently unoccupied space while not
+     * at max number of flies allowed for web
      * @param web - the Web object the fly is being generated in
      */
-    private static void generateFlies(Web web) {
-
+    private static void generateFlies(Web web)
+    {
         int row;  //row
         int col;  //column
 
         boolean generated;
         FlySize newSize;
+        
         //while there are still flies in the web to generate
-        while (FlySize.newSize() != null) {
+        while (FlySize.newSize() != null)
+        {
             newSize = FlySize.newSize();
             generated = false;
+            
             //gen random locations until empty spot is found, then add fly there
-            while (!generated) {
+            while (!generated)
+            {
                 row = random.nextInt(web.getWebLength());
                 col = random.nextInt(web.getWebLength());
-                if (getFly(row, col) == null) {
+                //generates a new fly
+                if (getFly(row, col) == null)
+                {
                     flies.add(new Fly(row, col, newSize));
                     Fly.getCurrentFlies();
                     generated = true;
-                }
-            }
-        }
-    }
+                }//end of if loop to generate new fly
+            }//end of gen random location to find empty spot
+        }//end of loop to run while there are still flies in the web to generate
+    }//end of generateFlies method
 
     /**
-     * diagnostics on current Fly information
+     * The debug method offers diagnostics on current Fly information
      */
-    private static void debug() {
+    private static void debug()
+    {
         System.out.println("DEBUG: Fly");
-        for (Fly fly : flies) {    //struggle
+        for (Fly fly : flies)
+        {    //struggle
             System.out.printf("#: %3d, ", flies.indexOf(fly) + 1);
             dump(fly);
-        }
+        }//end of enhanced for loop to display flies
         System.out.println("DEBUG: End Flies ------\n");
-    }
+    }//end of debug method
 
     /**
-     * dump the current information about the fly
+     * The dump method dumps the current information about the fly
      * @param fly - current fly that information is needed
      */
-    private static void dump(Fly fly) {
+    private static void dump(Fly fly)
+    {
         System.out.printf("ROW: %3d, COLUMN: %3d, SIZE: %6s, ENERGY: %3d, SCORE: %2d%n",
                 fly.locationRow,
                 fly.locationCol,
                 fly.size,
                 fly.vibrateEnergy,
                 fly.scoreValue);
-    }
-}
+    }//end of dump method
+}//end of Fly class
